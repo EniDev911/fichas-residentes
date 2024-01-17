920ca8ca047620849eb9ad85b5820a72158c960a
marco <enidev911@gmail.com>
2024-01-17 17:59:32 -0300

    up

--- app/handlers.js
+++ app/handlers.js
@@ -1,7 +1,59 @@
 const btnAgregar = document.querySelector('#btn_agregar');
+const form = document.querySelector("form");
+
+
+// form.addEventListener("submit", (event) => {
+// 	event.preventDefault();
+// })
 
 btnAgregar.onclick = () => {
 	const nombre = prompt('Nombre: ');
 	const rut = prompt('Rut: ');
 	const tel = prompt('Télefono: ');
 }
+
+
+let canvas = document.getElementById('main');
+let btnPDFout = document.getElementById('pdfout');
+
+btnPDFout.addEventListener('click', ()=> {
+	const doc = new jsPDF('p', 'pt', 'letter');
+	const margin = 20;
+	let scale = (doc.internal.pageSize.width - margin * 2) / document.body.clientWidth;
+	let scaleMobile = (doc.internal.pageSize.width - margin * 2) / document.body.getBoundingClientRect();
+// doc.text("Octonyan loves jsPDF", 35, 25);
+// doc.addImage("examples/images/Octonyan.jpg", "JPEG", 15, 40, 180, 180);
+	// checking
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  	// mobile
+  	doc.html(canvas, {
+		x: margin,
+    	y: margin,
+    	html2canvas: {
+      		scale: scaleMobile,
+    	},
+    	callback: function(doc) {
+      		doc.output('dataurlnewwindow',{ filename: 'pdf.pdf'})
+    	}
+  	})
+	} else {
+  // PC
+  // doc.html(canvas, {
+  //   x: margin,
+  //   y: margin,
+  //   html2canvas: {
+  //     scale: scale,
+  //     foreignObjectRendering: true,
+  //   },
+  //   callback: function(doc) {
+
+  //     doc.output('dataurlnewwindow',{ filename: 'pdf.pdf'})
+  //   }
+  // })
+
+  // Other solution
+	// doc.setFontSize(40);
+  	doc.text("Condominio Espacio Uno III", 135, 25);
+  	doc.output('dataurlnewwindow',{ filename: 'pdf.pdf'})
+	}
+})
